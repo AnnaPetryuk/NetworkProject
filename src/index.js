7fb66ff4b536fb4b169f17930ce9a5551ecba5c0
PetrukAnna <annyap@ukr.net>
2020-11-20 10:23:44 +0200

    Added observer pattern for state changing

--- src/index.js
+++ src/index.js
@@ -1,21 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import state from './redux/state'
-import { addPost } from './redux/state'
-import { rerenderEntireTree } from './render';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import state, { subscribe } from "./redux/state";
+import { addPost, updateNewPostText } from "./redux/state";
+
+let rerenderEntireTree = (state) => {
+    ReactDOM.render(
+        <React.StrictMode>
+            <App
+                state={state}
+                addPost={addPost}
+                updateNewPostText={updateNewPostText}
+            />
+        </React.StrictMode>,
+        document.getElementById("root")
+    );
+};
 
 rerenderEntireTree(state);
-// let rerenderEntireTree = () => {
-//   ReactDOM.render(
-//     <React.StrictMode> 
-//       <App state={state} addPost={ addPost }/>
-//     </React.StrictMode>,
-//     document.getElementById('root')
-//   );
-// }
+subscribe(rerenderEntireTree);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,4 +1,6 @@
-import { rerenderEntireTree } from "../render";
+let rerenderEntireTree = () => {
+    console.log("State changed");
+};
 
 let state = {
     profilePage: {
@@ -7,7 +9,8 @@ let state = {
             { mesID: 2, message: "It is my first post", likeCount: 30},
             { mesID: 3, message: "Thank you", likeCount: 10},
             { mesID: 4, message: "Hi", likeCount: 17}
-        ]
+        ],
+        newPostText: "AAAAAAA"
     },
     dialogsPage: {
         messagesData: [
@@ -27,15 +30,31 @@ let state = {
     }
 }
 
-export let addPost = (postMessage) => {
+// Для того, щоб перевірити як змінюється стан можна
+// додати його у windows
+window.state = state;
+
+export const addPost = () => {
     let newPost = { 
         mesId: 5,
-        message: postMessage,
+        message: state.profilePage.newPostText,
         likeCount: 0
     };
 
     state.profilePage.posts.push(newPost);
+    state.profilePage.newPostText = '';
     rerenderEntireTree(state);
 }
 
+export const updateNewPostText = (newPostText) => {
+    state.profilePage.newPostText = newPostText;
+    rerenderEntireTree(state);
+}
+
+export const subscribe = (observer) => {
+    rerenderEntireTree = observer; // Pattern observer (по цьому патерну працює addEventListener)
+}
+
 export default state;
+
+// store - OOP
