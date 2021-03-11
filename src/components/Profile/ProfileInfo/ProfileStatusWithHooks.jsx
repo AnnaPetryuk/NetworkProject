e4ad7bb19f4845d55f2ca29dcb0a5db00877ade7
PetrukAnna <anna.petriuk@infotech.gov.ua>
2021-03-11 19:19:44 +0200

    Added useEffect hook

---
+++ src/components/Profile/ProfileInfo/ProfileStatusWithHooks.jsx
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from 'react';
+
+const ProfileStatusWithHooks = (props) => {
+    let [editMode, setEditMode] = useState(false);
+    let [status, setStatus] = useState(props.status);
+    
+    // Запускає після того, коли функція отрисуется
+    useEffect(() => {
+        setStatus(props.status);
+        // якщо змінюється props.status => перемалюй компоненту
+    }, [props.status]) // пустой масив => отрисуется 1 раз // так указывать нельзя
+
+    const activateEditMode = () => {
+        setEditMode(true);
+    }
+
+    const deactivateEditMode = () => {
+        setEditMode(false);
+        props.updateStatus(status);
+    }
+
+    const onStatusChange = (e) => {
+        setStatus(e.currentTarget.value);
+    }
+
+    return (
+        <div>
+            {!editMode
+                ? 
+                <div>
+                    <span onDoubleClick={activateEditMode}>{props.status || '----' }</span>
+                </div>
+                :
+                <div>
+                    <input onChange={onStatusChange} autoFocus={true} onBlur={deactivateEditMode} value={status}/>
+                </div>
+            }
+        </div>
+    );
+};
+
+export default ProfileStatusWithHooks;
