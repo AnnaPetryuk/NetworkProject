0f05ae42d65bd699bf500ee90fb9730279199d19
Anna Petriuk <anna.petriuk@infotech.gov.ua>
2021-05-12 19:30:02 +0300

    upload profile photo

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -2,10 +2,17 @@ import React from 'react';
 import Preloader from '../../common/Preloader/Preloader';
 import styles from "./ProfileInfo.module.css";
 import ProfileStatusWithHooks from './ProfileStatusWithHooks';
+import userPhoto from "../../../assets/images/user.jpg";
 
-const ProfileInfo = ({profile, status, updateStatus}) => {
+const ProfileInfo = ({isOwner, profile, status, updateStatus, savePhoto}) => {
     if(!profile) {
         return <Preloader/>
+    }
+
+    const onMainPhotoSelected = (e) => {
+        if(e.target.files.length){
+            savePhoto(e.target.files[0])
+        }
     }
 
     return (
@@ -14,7 +21,8 @@ const ProfileInfo = ({profile, status, updateStatus}) => {
                 <img src="https://www.publicdomainpictures.net/pictures/320000/nahled/background-image.png" alt={"Background"}/>
             </div> */}
             <div className={styles.descriptionBlock}>
-                <img src={profile.photos.large} alt="ProfileImg"/>
+                <img src={profile.photos.large || userPhoto} alt="ProfileImg"/>
+                {isOwner && <input type={"file"} name="file" onChange={onMainPhotoSelected}/>}
                 <ProfileStatusWithHooks status={status} updateStatus={updateStatus}/>
                 {/* ava + description */}
             </div>
--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -4,6 +4,7 @@ const ADD_POST = 'ADD-POST';
 const DELETE_POST = 'DELETE_POST';
 const SET_USER_PROFILE = 'SET_USER_PROFILE';
 const SET_STATUS = 'SET_STATUS';
+const SAVE_PHOTO_SUCCESS = 'SAVE_PHOTO_SUCCESS';
 
 let initialState = {
     posts: [
@@ -44,6 +45,11 @@ const profileReducer = (state = initialState, action) => {
                 ...state,
                 status: action.status
             }  
+        case SAVE_PHOTO_SUCCESS:
+            return {
+                ...state,
+                profile: { ...state.profile, photos: action.photos }
+            }
         default:
             return state;
     }
@@ -77,6 +83,13 @@ export const setStatus = (status) => {
     }
 }
 
+export const savePhotoSuccess = (photos) => {
+    return { 
+        type: SAVE_PHOTO_SUCCESS, 
+        photos
+    }
+}
+
 export const getUserProfile = (userId) => async (dispatch) => {
     let response = await userAPI.getProfile(userId);
     
@@ -94,6 +107,14 @@ export const updateStatus = (status) => async (dispatch) => {
     
     if(response.data.resultCode === 0) {
         dispatch(setStatus(status));
+    }
+}
+
+export const savePhoto = (file) => async (dispatch) => {
+    let response = await profileAPI.savePhoto(file);
+    
+    if(response.data.resultCode === 0) {
+        dispatch(savePhotoSuccess(response.data.data.photos));
     }
 }
 
